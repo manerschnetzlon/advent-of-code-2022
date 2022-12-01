05683339128506711ec8139ab4dd8f618a1db440
maner <manon.schnetzler@pennylane.com>
2022-12-01 09:13:32 +0100

    day01 ok

--- day01.rb
+++ day01.rb
@@ -1,2 +1,4 @@
 file = File.open('inputs/input_day01.txt')
-file_array = file.read.split("\n")
+file_array = file.read.split("\n\n")
+
+p file_array.map { _1.split("\n").map(&:to_i).sum }.max(3).sum
