5db59fddf49536e1a7f3df40a3d209ad23b8f432
maner <manon.schnetzler@pennylane.com>
2022-12-04 11:45:10 +0100

    day04 ok

--- day04.rb
+++ day04.rb
@@ -1,2 +1,20 @@
+# frozen_string_literal: true
+
 file = File.open('inputs/input_day04.txt')
 file_array = file.read.split("\n")
+
+ranges = file_array.map { _1.split(',') }
+
+overlaps_part1 = ranges.select do |range|
+  parts = range.map { _1.split('-') }
+  (parts[0][0].to_i <= parts[1][0].to_i && parts[0][1].to_i >= parts[1][1].to_i) ||
+    (parts[0][0].to_i >= parts[1][0].to_i && parts[0][1].to_i <= parts[1][1].to_i)
+end
+
+overlaps_part2 = ranges.reject do |range|
+  parts = range.map { _1.split('-') }
+  (((parts[0][0].to_i)..(parts[0][1].to_i)).to_a & ((parts[1][0].to_i)..(parts[1][1].to_i)).to_a).empty?
+end
+
+p "part1: #{overlaps_part1.count}"
+p "part2: #{overlaps_part2.count}"
