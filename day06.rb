ee791895bb2c0d8ef59c88965fae11192a3b4ec0
maner <manon.schnetzler@pennylane.com>
2022-12-06 13:14:07 +0100

    day06 ok

--- day06.rb
+++ day06.rb
@@ -1,2 +1,15 @@
-file = File.open('inputs/input_day06.txt')
-file_array = file.read.split("\n")
+# frozen_string_literal: true
+
+signal = File.open('inputs/input_day06.txt').read
+
+def find_marker(signal, marker_length)
+  received = []
+  signal.chars do |c|
+    received << c
+    break if received.last(marker_length).uniq.count == marker_length
+  end
+  received.count
+end
+
+p "part1: #{find_marker(signal, 4)}"
+p "part1: #{find_marker(signal, 14)}"
