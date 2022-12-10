5c57be616c1551a5bed13f911cf06b468afb4039
Manon Schnetzler <manon.schnetzler@pennylane.com>
2022-12-10 20:46:38 +0100

    day09 ok

--- day09.rb
+++ day09.rb
@@ -2,16 +2,24 @@ file = File.open('inputs/input_day09.txt')
 file_array = file.read.split("\n")
 
 class Rope
-  attr_reader :head, :tail, :visited_positions
+  attr_reader :head, :part1, :part2, :part3, :part4, :part5, :part6, :part7, :part8, :tail, :visited_tail_positions
 
   def initialize
     @head = { x: 0, y: 0 }
+    @part1 = { x: 0, y: 0 }
+    @part2 = { x: 0, y: 0 }
+    @part3 = { x: 0, y: 0 }
+    @part4 = { x: 0, y: 0 }
+    @part5 = { x: 0, y: 0 }
+    @part6 = { x: 0, y: 0 }
+    @part7 = { x: 0, y: 0 }
+    @part8 = { x: 0, y: 0 }
     @tail = { x: 0, y: 0 }
-    @visited_positions = [ {x: 0, y: 0}]
+    @visited_tail_positions = [ {x: 0, y: 0} ]
   end
 
-  def head_tail_touching
-    ((@head[:x] - @tail[:x]).abs <= 1) && ((@head[:y] - @tail[:y]).abs <= 1)
+  def head_tail_touching(h, t)
+    ((h[:x] - t[:x]).abs <= 1) && ((h[:y] - t[:y]).abs <= 1)
   end
 
   def move_head(direction)
@@ -26,30 +34,55 @@ class Rope
     end
   end
 
-  def move_tail
-    return if head_tail_touching
+  def move(h, t)
+    return if head_tail_touching(h, t)
 
-    if @head[:y] == @tail[:y]
-      @tail[:x] += @head[:x] > @tail[:x] ? 1 : -1
-    elsif @head[:x] == @tail[:x]
-      @tail[:y] += @head[:y] > @tail[:y] ? 1 : -1
+    if h[:y] == t[:y]
+      t[:x] += h[:x] > t[:x] ? 1 : -1
+    elsif h[:x] == t[:x]
+      t[:y] += h[:y] > t[:y] ? 1 : -1
     else
-      @tail[:x] += @head[:x] > @tail[:x] ? 1 : -1
-      @tail[:y] += @head[:y] > @tail[:y] ? 1 : -1
+      t[:x] += h[:x] > t[:x] ? 1 : -1
+      t[:y] += h[:y] > t[:y] ? 1 : -1
     end
 
-    @visited_positions << @tail.clone
+    @visited_tail_positions << @tail.clone if t == @tail
   end
 end
 
-rope = Rope.new
 
-file_array.each do |line|
-  dir, steps = line.split
-  steps.to_i.times do 
-    rope.move_head(dir)
-    rope.move_tail
+def part1(input)
+  rope = Rope.new
+  input.each do |line|
+    dir, steps = line.split
+    steps.to_i.times do
+      rope.move_head(dir)
+      rope.move(rope.head, rope.tail)
+    end
   end
+  rope.visited_tail_positions.uniq.count
 end
 
-p rope.visited_positions.uniq.count
+def part2(input)
+  rope = Rope.new
+  input.each do |line|
+    dir, steps = line.split
+    steps.to_i.times do
+      rope.move_head(dir)
+      rope.move(rope.head, rope.part1)
+      rope.move(rope.part1, rope.part2)
+      rope.move(rope.part2, rope.part3)
+      rope.move(rope.part3, rope.part4)
+      rope.move(rope.part4, rope.part5)
+      rope.move(rope.part5, rope.part6)
+      rope.move(rope.part6, rope.part7)
+      rope.move(rope.part7, rope.part8)
+      rope.move(rope.part8, rope.tail)
+    end
+  end
+  rope.visited_tail_positions.uniq.count
+end
+
+p "part1: #{part1(file_array)}"
+p "part2: #{part2(file_array)}"
+
