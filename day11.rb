936f5a29f632494a41160a84bedd290134c6868f
Manon Schnetzler <manon.schnetzler@pennylane.com>
2022-12-13 22:51:23 +0100

    day11 part2

--- day11.rb
+++ day11.rb
@@ -4,14 +4,18 @@ file = File.open('inputs/input_day11.txt')
 file_array = file.read.split("\n\n")
 
 class Monkey
-  attr_reader :number
+  attr_reader :number, :operation, :tester, :monkey_success, :monkey_failure
   attr_accessor :items, :inspections
 
-  def initialize(number, keep_away)
-    @number = number
-    @items = []
+  def initialize(monkey_data)
+    @number = monkey_data[:number]
+    @items = monkey_data[:items]
+    @operation = monkey_data[:operation]
+    @tester = monkey_data[:tester]
+    @monkey_success = monkey_data[:monkey_success]
+    @monkey_failure = monkey_data[:monkey_failure]
     @inspections = 0
-    @keep_away = keep_away
+    @keep_away = monkey_data[:keep_away]
   end
 
   def self.find(monkey_number, keep_away)
@@ -26,14 +30,13 @@ class KeepAway
     @input = input
     @monkeys = []
     @current_monkey = []
+    @relief = 1
   end
 
   def game(rounds)
     setup
     rounds.times do
-      @input.each do |input|
-        turn(input)
-      end
+      @monkeys.each { |monkey| turn(monkey) }
     end
     display_result
   end
@@ -46,39 +49,46 @@ class KeepAway
 
   def setup
     @input.each do |input_turn|
-      bloc = input_turn.split("\n").map { _1.scan(/\d+/) }
-      items = bloc[1].map(&:to_i)
-      monkey = Monkey.new(bloc[0].first.to_i, self)
-      monkey.items = items
+      monkey = input_turn.split("\n")
+      monkey_data = parse(monkey)
+      monkey = Monkey.new(monkey_data)
       @monkeys << monkey
     end
+    @relief = @monkeys.map(&:tester).inject(:*)
   end
 
-  def turn(input_turn)
-    elements = parse(input_turn)
-    @current_monkey = Monkey.find(elements[:num], self)
-    @current_monkey.items.each do |item|
-      @current_monkey.inspections += 1
-      multiplier = elements[:multi].empty? ? item : elements[:multi].first.to_i
-      item = elements[:operator] == '+' ? (item + multiplier) / 3 : (item * multiplier) / 3
-      next_monkey_num = (item % elements[:tester]).zero? ? elements[:monkey_true] : elements[:monkey_false]
+  def parse(monkey)
+    {
+      number: monkey[0].scan(/\d+/).first.to_i,
+      items: monkey[1].scan(/\d+/).map(&:to_i),
+      operation: monkey[2].scan(/old .+/).first,
+      tester: monkey[3].scan(/\d+/).first.to_i,
+      monkey_success: monkey[4].scan(/\d+/).first.to_i,
+      monkey_failure: monkey[5].scan(/\d+/).first.to_i,
+      keep_away: self
+    }
+  end
+
+  def turn(current_monkey)
+    current_monkey.items.each do |item|
+      current_monkey.inspections += 1
+      item = operation(item, current_monkey)
+      # item /= 3
+      item %= @relief
+      next_monkey_num = find_next_monkey(item, current_monkey)
       Monkey.find(next_monkey_num, self).items << item
     end
-    @current_monkey.items = []
+    current_monkey.items = []
   end
 
-  def parse(input)
-    bloc = input.split("\n")
-    {
-      num: bloc[0].scan(/\d+/).first.to_i,
-      operator: bloc[2].scan(/(\+|\*)/).flatten.first,
-      multi: bloc[2].scan(/\d+/),
-      tester: bloc[3].scan(/\d+/).first.to_i,
-      monkey_true: bloc[4].scan(/\d+/).first.to_i,
-      monkey_false: bloc[5].scan(/\d+/).first.to_i
-    }
+  def operation(item, monkey)
+    eval monkey.operation.gsub('old', item.to_s)
+  end
+
+  def find_next_monkey(item, monkey)
+    (item % monkey.tester).zero? ? monkey.monkey_success : monkey.monkey_failure
   end
 end
 
 keep_away = KeepAway.new(file_array)
-keep_away.game(20)
+keep_away.game(10_000)
