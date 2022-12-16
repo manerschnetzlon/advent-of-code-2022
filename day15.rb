b3f7c8c1397fd4b29c0ce15c4fc755c6e4e9571a
Manon Schnetzler <manon.schnetzler@pennylane.com>
2022-12-16 18:07:06 +0100

    day15 part1 with help

--- day15.rb
+++ day15.rb
@@ -1,2 +1,29 @@
+# frozen_string_literal: true
+
+require 'set'
 file = File.open('inputs/input_day15.txt')
 file_array = file.read.split("\n")
+
+def distance(point_a, point_b)
+  (point_a[0] - point_b[0]).abs + (point_a[1] - point_b[1]).abs
+end
+
+sensors = Set.new
+beacons = Set.new
+
+file_array.map { |line| line.scan(/-?\d+/).map(&:to_i) }
+          .map do |coord|
+            sensor = coord[0..1]
+            beacon = coord[2..]
+            sensors << [sensor, distance(sensor, beacon)]
+            beacons << beacon
+          end
+
+excluded_positions = Set.new
+(-10_000_000..10_000_000).each do |x|
+  point = [x, 2_000_000]
+  sensors.each do |position, distance|
+    excluded_positions << point if distance(position, point) <= distance && !beacons.include?(point)
+  end
+end
+p "part1: #{excluded_positions.count}"
