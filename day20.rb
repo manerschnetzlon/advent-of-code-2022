dbf40010a32401a59a808b93bbe172cd2dac9ab8
Manon Schnetzler <manon.schnetzler@pennylane.com>
2022-12-31 18:17:27 +0100

    day20 part1 ok

--- day20.rb
+++ day20.rb
@@ -1,2 +1,28 @@
+# frozen_string_literal: true
+
 file = File.open('inputs/input_day20.txt')
-file_array = file.read.split("\n")
+file_array = file.read.split("\n").map(&:to_i)
+
+def decrypt(list)
+  list.length.times do |i|
+    list.rotate! until list.first[1] == i
+    head, *tail = list
+    tail.rotate!(head[0])
+    tail.unshift(head)
+    list = tail.rotate!(-head[0])
+  end
+  list.rotate! until list.first[0] == 0
+  list.map(&:first)
+end
+
+def find_at_index(index, list)
+  length = list.length
+  while index >= length
+    index -= length
+  end
+  list[index]
+end
+
+list = file_array.map.with_index { |num, index| [num, index] }
+decrypted_list = decrypt(list)
+p find_at_index(1000, decrypted_list) + find_at_index(2000, decrypted_list) + find_at_index(3000, decrypted_list)
