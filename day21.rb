13c37f33ff1cf95a6b8577b0da56d61530cd6767
Manon Schnetzler <manon.schnetzler@pennylane.com>
2023-01-01 14:20:03 +0100

    day21 part1 ok

--- day21.rb
+++ day21.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 file = File.open('inputs/input_day21.txt')
-file_array = file.read.split("\n")
+input = file.read.split("\n").map { |line| line.split(': ') }.to_h
+
+formula = input['root']
+while /\w{4}/.match?(formula)
+  formula.gsub!(/\w{4}/) { |match| "(#{input[match]})" }
+end
+
+p "part1: #{eval formula}"
