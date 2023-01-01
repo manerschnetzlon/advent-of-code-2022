71c23b10ccbf022e3987aaaa1b76fdc0677e1d05
Manon Schnetzler <manon.schnetzler@pennylane.com>
2023-01-01 16:24:00 +0100

    day21 ok

--- day21.rb
+++ day21.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
 file = File.open('inputs/input_day21.txt')
-input = file.read.split("\n").map { |line| line.split(': ') }.to_h
+file_array = file.read.split("\n")
 
-formula = input['root']
-while /\w{4}/.match?(formula)
-  formula.gsub!(/\w{4}/) { |match| "(#{input[match]})" }
+def part1(input)
+  formula = input['root']
+  while /\w{4}/.match?(formula)
+    formula.gsub!(/\w{4}/) { |match| "(#{input[match]})" }
+  end
+  formula
 end
 
-p "part1: #{eval formula}"
+def part2(input)
+  formula = input['root'].gsub!('+', '==')
+  while /\w{4}/.match?(formula)
+    formula.gsub!(/\w{4}/) { |match| "(#{input[match]})" }
+  end
+  new_formula = formula.split(' == ').map { |f| f.include?('h') ? f : (eval f) }
+  new_formula.partition { |f| f.instance_of?(String) }.map(&:first)
+end
+
+input = file_array.map { |line| line.split(': ') }.to_h
+p "part1: #{eval part1(input)}"
+
+input2 = file_array.map { |line| line.gsub('humn', 'h').split(': ') }.to_h
+p "part2: #{part2(input2).join(' = ')}" # use online equation solver
